bbf273d5ad2206f853b6af74b40c21e5f02cd4b8
Anoop Pandey <apandey@getquantic.com>
2023-09-23 19:01:50 +0530

    changes done

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -13,8 +13,8 @@ function Github() {
 
     return (
         <>
-            <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">Github followers : {data.followers}
-            <img src={data.avatar_url} alt="Git Picture" width={300} className="text-center" />
+            <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl img_center">Github followers : {data.followers}
+            <img src={data.avatar_url} alt="Git Picture" width={300} />
             </div>
         </>
     )
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -33,12 +33,6 @@ export default function Home() {
                     <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
                 </div>
             </aside>
-
-            <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-            </div>
-
-            <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
         </div>
     );
 }
